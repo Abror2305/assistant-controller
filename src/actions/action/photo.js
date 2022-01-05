2e8b164676bee9280b415ba6dcfc7e6d69cd547c
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-01-06 00:19:49 +0500

    add reply message and connect chanel

--- src/actions/action/photo.js
+++ src/actions/action/photo.js
@@ -1,32 +1,42 @@
 const { composer, middleware } = require("../../core/bot");
 const { Markup } = require("telegraf")
 
-const admins = require("../../core/admins.json")
-
 let senderId = 0
 
 composer.on("photo",ctx=>{
-    ctx.replyWithHTML("<b>Qabul qilindi ✅</b>").then()
+    const username =ctx.from.username
+    senderId = ctx.from.id
+    const text = `<b>Jonatuvchi</b>\n@${username}`
+    let photo = ctx.message.photo[0].file_id
+    let msid = ctx.message.message_id
 
+
+    ctx.reply(`<b>Qabul qilindi ✅</b>`,{
+        reply_to_message_id:msid,
+        parse_mode:"HTML"
+    }).then()
     const keyboard = Markup.inlineKeyboard([
         Markup.callbackButton("Tasdiqlash ✅","true"),
         Markup.callbackButton("Xato ❌","false")
 
     ])
-    senderId = ctx.message.from.id
-    const text = `<b>Jonatuvchi</b>\n@${ctx.from.user_id}`
-    let photo = ctx.message.photo[0].file_id
-    for (let admin of admins) {
-        ctx.telegram.sendPhoto(admin,photo,{
-            caption:text,
-            parse_mode: "HTML",
-            reply_markup: keyboard
-        }).then()
-    }
+
+    ctx.telegram.sendPhoto(-1001628048404,photo,{
+        caption:text,
+        parse_mode: "HTML",
+        reply_markup: keyboard}).then()
 })
+
+
 composer.action("true",ctx => {
     ctx.telegram.sendMessage(senderId,"Sizning vazifangiz tasdiqlandi").then()
     ctx.editMessageCaption("<b>Tasdiqlandi ✅</b>",{parse_mode:"HTML"}).then()
+})
+
+
+composer.action("false",ctx=>{
+    ctx.telegram.sendMessage(senderId,"Sizning vazifangiz xato. Iltimos qayta urunib koring!").then()
+    ctx.editMessageCaption("<b>Xato</b>>",{parse_mode:"HTML"}).then()
 })
 
 middleware(composer);
