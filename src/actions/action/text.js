241e60af63f5b37f90e394e5f7c49bf9561176e9
abdugani0523 <abdugani0523@gmail.com>
2022-01-06 00:15:55 +0500

    Added more features (beta)

---
+++ src/actions/action/text.js
@@ -0,0 +1,21 @@
+const { composer, middleware } = require("../../core/bot");
+const { textToAdmin, replyedMessage } = require("../messages");
+const env = require('../../core/env')
+const { acceptOrIgnore, showCode } = require('../keys')
+composer.on("text", async (ctx) => {
+  let message = ctx.message.text;
+  if (message.match(/^#code/g) && ctx.message.reply_to_message) {
+    await ctx.telegram.sendMessage(env.ADMIN_CHANNEL, textToAdmin(ctx, message), {
+        reply_markup: acceptOrIgnore,
+        parse_mode: "HTML"
+    }).then()
+    await ctx.telegram.deleteMessage(env.CONFESSION, ctx.message.message_id);
+      await ctx.reply(replyedMessage(ctx,'text'), {
+          reply_markup: showCode,
+          reply_to_message_id: ctx.message.reply_to_message.message_id,
+          parse_mode: "HTML"
+      })
+  }
+});
+
+middleware(composer);
--- src/actions/commands/help.js
+++ src/actions/commands/help.js
@@ -1,10 +1,8 @@
 const { composer, middleware } = require("../../core/bot");
 const { help } = require("../messages");
 
-composer.action("help", async (ctx) => {
-  await ctx.editMessageText(help, {
-    parse_mode: "HTML",
-  });
+composer.help((ctx) => {
+  ctx.replyWithHTML(help).then();
 });
 
 middleware(composer);
