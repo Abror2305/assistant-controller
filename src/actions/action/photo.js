f8a482e7a830276b8a4c50fdc7745844492c770f
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-01-06 00:43:04 +0500

    remove admins.json and add comments

--- src/actions/action/photo.js
+++ src/actions/action/photo.js
@@ -1,39 +1,43 @@
 const { composer, middleware } = require("../../core/bot");
 const { Markup } = require("telegraf")
-
+const env = require('../../core/env')
 let senderId = 0
 
-composer.on("photo",ctx=>{
+composer.on("photo", async ctx=>{
     const username =ctx.from.username
     senderId = ctx.from.id
     const text = `<b>Jonatuvchi</b>\n@${username}`
     let photo = ctx.message.photo[0].file_id
     let msid = ctx.message.message_id
 
-
-    ctx.reply(`<b>Qabul qilindi ✅</b>`,{
-        reply_to_message_id:msid,
-        parse_mode:"HTML"
-    }).then()
+    // inlineKeyboards
     const keyboard = Markup.inlineKeyboard([
         Markup.callbackButton("Tasdiqlash ✅","true"),
         Markup.callbackButton("Xato ❌","false")
 
     ])
 
-    ctx.telegram.sendPhoto(-1001628048404,photo,{
+    // Qabul qilinganini foydalanuvchiga aytish
+    await ctx.reply(`<b>Qabul qilindi ✅</b>`,{
+        reply_to_message_id:msid,
+        parse_mode:"HTML"
+    }).then()
+
+    // Adminlarning kanaliga tashlash
+    await ctx.telegram.sendPhoto(env.ADMINS_CH,photo,{
         caption:text,
         parse_mode: "HTML",
         reply_markup: keyboard}).then()
 })
 
 
+// Tasdiqlash
 composer.action("true",ctx => {
     ctx.telegram.sendMessage(senderId,"Sizning vazifangiz tasdiqlandi").then()
     ctx.editMessageCaption("<b>Tasdiqlandi ✅</b>",{parse_mode:"HTML"}).then()
 })
 
-
+// Reject
 composer.action("false",ctx=>{
     ctx.telegram.sendMessage(senderId,"Sizning vazifangiz xato. Iltimos qayta urunib koring!").then()
     ctx.editMessageCaption("<b>Xato</b>>",{parse_mode:"HTML"}).then()
