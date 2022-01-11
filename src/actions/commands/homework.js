82edc4f5781482336c20cf9325aed3cc44d9b53b
abror2305 <abroralisherov90@gmail.com>
2022-01-11 19:23:00 +0500

    homework.js commented connect and end methods

--- src/actions/commands/homework.js
+++ src/actions/commands/homework.js
@@ -7,16 +7,16 @@ composer.on("channel_post", async (ctx) => {
     let text = content.text ?? content.caption ?? '';
     let file_id = !(content["photo"]) ? " " : content["photo"][0]["file_id"];
     if (text.match(/^#homework/gi)){
-        connection.connect((err) => {
-            if(err) throw err
-            console.log("Connected")
-        })
+        // connection.connect((err) => {
+        //     if(err) throw err
+        //     console.log("Connected")
+        // })
         let iquery = `INSERT INTO Homework(message_id, file_id) VALUES (${content.message_id}, "${file_id}");`
         connection.query(iquery,err => {
             if(err) throw err;
             console.log("Query Executed")
         })
-        connection.end()
+        // connection.end()
     }
 })
 middleware(composer)
