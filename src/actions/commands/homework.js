8173d441e2c292dcc02b9f1d388f24f26b2d0129
Abror Alisherov <74832377+abror2305@users.noreply.github.com>
2022-01-16 17:26:15 +0500

    Homework completed

--- src/actions/commands/homework.js
+++ src/actions/commands/homework.js
@@ -5,25 +5,26 @@ const { getLastID } = require("../lib");
 const group = require('../../db/group.json')
 
 composer.on("channel_post", async (ctx) => {
-  console.log(ctx.update.channel_post);
-  console.log(typeof group);
-  console.log(group);
 
-  // // Get most needed data
-  // let content = ctx.update.channel_post;
-  // let text = content.text ?? content.caption ?? "";
-  //
-  // // Validation
-  // if (text.match(/^#homework/gi)) {
-  //   let query = `INSERT INTO homework ( message_id ) VALUES ( ${content.message_id} );`;
-  //
-  //   connection.query(query, (err) => {
-  //     if (err) throw err;
-  //   });
-  //   connection.commit();
-  //
-  //   let currentId = getLastID("homework");
-  //   homeworkSaved(currentId);
-  // }
+    // Get most needed data
+    let content = ctx.update.channel_post;
+    let text = content.text ?? content.caption ?? "";
+
+    // Validation
+    if (text.match(/^#homework/gi)) {
+      if (group[ctx.update.channel_post.chat.id]) {
+      let query = `INSERT INTO homework ( message_id ) VALUES ( ${content.message_id} );`;
+
+      connection.query(query, (err) => {
+        if (err) throw err;
+      });
+      connection.commit();
+
+      let currentId = getLastID("homework");
+      homeworkSaved(currentId);
+    }else{
+        await ctx.replyWithMarkdown("*Sizning kanalingiz hali ro'yhatdan o'tmagan*").catch(()=> {console.log("Admin emas odam ishga tushirdi!")});
+      }
+  }
 });
 middleware(composer);
