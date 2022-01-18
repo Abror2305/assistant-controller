fe8242b5130c36adef36bb11c34c1306dc336d02
Abror Alisherov <74832377+abror2305@users.noreply.github.com>
2022-01-19 01:02:01 +0500

    add new model.sql

--- src/db/model.sql
+++ src/db/model.sql
@@ -1,5 +1,18 @@
-DROP TABLE IF EXISTS `answer`;
-CREATE TABLE `answer` (
+DROP TABLE IF EXISTS `admins`;
+
+CREATE TABLE `admins` (
+  `id` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `admins` WRITE;
+
+
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `answers`;
+
+
+CREATE TABLE `answers` (
   `id` int(15) NOT NULL AUTO_INCREMENT,
   `from_id` int(50) NOT NULL,
   `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -10,19 +23,34 @@ CREATE TABLE `answer` (
   `status` tinyint(1) DEFAULT NULL,
   `replaced_message_id` int(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-LOCK TABLES `answer` WRITE;
+
+
+LOCK TABLES `answers` WRITE;
+
+
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `homework`;
-CREATE TABLE `homework` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
-  `channel_id` int(15) NOT NULL,
-  `message_id` int(15) NOT NULL,
-  PRIMARY KEY (`id`)
+
+DROP TABLE IF EXISTS `groups`;
+
+
+CREATE TABLE `groups` (
+  `share_point` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `discussion` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin_channel` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `groups` WRITE;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `homeworks`;
+CREATE TABLE `homeworks` (
+  `channel_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message_id` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-LOCK TABLES `homework` WRITE;
+LOCK TABLES `homeworks` WRITE;
 UNLOCK TABLES;
